e975c97b76fcb9eee53df32189da035df68594fe
roh31t <rohit.gusknp2023@gmail.com>
2025-06-30 20:50:03 +0530

    Delete bookingjini directory

--- bookingjini/next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    env: {
-      GOOGLE_CLIENT_ID: "",
-      GOOGLE_CLIENT_SECRET: "",
-      MONGODB_URI: "",
-      NEXTAUTH_URL: "http://localhost:3000",
-      NEXTAUTH_SECRET: "your-secret-key-here"
-    }
-  }
